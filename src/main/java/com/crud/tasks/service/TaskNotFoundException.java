94dc876a18b18ce2861a9379e5273ca218e4c1d8
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-06-18 12:14:12 +0200

    Add Kodilla exercises: Kodilla module 19.3

--- src/main/java/com/crud/tasks/service/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.crud.tasks.service;
-
-public class TaskNotFoundException extends RuntimeException {
-    public TaskNotFoundException(final String message) {
-        super(message);
-    }
-}
